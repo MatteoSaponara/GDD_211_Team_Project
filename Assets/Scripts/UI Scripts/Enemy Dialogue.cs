6a272e555ec11d447d38101d8d1d7f3f886e6258
MatteoSaponara <saponaramatteo30@gmail.com>
2026-07-31 17:54:08 -0400

    Adjusted Typing speed
    - Set Letter Delay to 0.075

--- Assets/Scripts/UI Scripts/Enemy Dialogue.cs
+++ Assets/Scripts/UI Scripts/Enemy Dialogue.cs
@@ -12,11 +12,11 @@ namespace game
         [Tooltip("Text component of the dialogue.")]
         [SerializeField] private TMP_Text dialogueText;
 
-        [Header("")]
+        [Header("Typewriter Effect")]
         [Tooltip("Time between each letter being typed.")]
         [SerializeField] private float letterDelay = 0.1f;
 
-        //
+        // Reference to TypeDialogue coroutine
         private Coroutine currentDialogue;
 
         // Sets texts (and panel)
